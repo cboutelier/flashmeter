36ac885d1150506572e6b867c0af0eed8d0c0190
admin <cedric@boutelier.com>
2020-11-19 15:25:15 +0100

    First read of light intensity, result sent to the console

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <BH1750.h>
+
+#define SCL_PIN 22
+#define SDA_PIN 21
+
+BH1750 lightMeter;
+
+void setup()
+{
+
+  //Alway initiate a serial connection...
+  Serial.begin(9600);
+
+  //Specific initialization of the Wire library: because the bh1750 lib does not do it, and because the board uses non standard pins.
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  //Start the sensor in high res mode.
+  lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
+
+  Serial.println(F("Setup done"));
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+
+  //get the value of the sensor
+  float lux = lightMeter.readLightLevel(true);
+  Serial.print(F("Light: "));
+  Serial.print(lux);
+  Serial.println(F(" lx"));
+
+  delay(5000);
 }
